1ee91887bf1f27c37379cf4a8223e56f3a43f21e
greforce <greforce76@gmail.com>
2018-07-24 16:55:42 +0300

    initial commit

---
+++ tests/main.js
@@ -0,0 +1,23 @@
+import assert from "assert";
+// import { assert } from 'meteor/practicalmeteor:chai';
+
+import '../imports/api/tasks.tests.js';
+
+describe("myapp", function () {
+  it("package.json has correct name", async function () {
+    const { name } = await import("../package.json");
+    assert.strictEqual(name, "myapp");
+  });
+
+  if (Meteor.isClient) {
+    it("client is not server", function () {
+      assert.strictEqual(Meteor.isServer, false);
+    });
+  }
+
+  if (Meteor.isServer) {
+    it("server is not client", function () {
+      assert.strictEqual(Meteor.isClient, false);
+    });
+  }
+});
